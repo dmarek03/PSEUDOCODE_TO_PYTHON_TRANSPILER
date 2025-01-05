440ef06516b1a2754235a88e85491d419db18ac4
dmarek03 <domarek@student.agh.edu.pl>
2025-01-05 15:38:58 +0100

    Added visitDeclaration method

--- PseudocodeToPythonTranspiler/translator.py
+++ PseudocodeToPythonTranspiler/translator.py
@@ -11,6 +11,67 @@ class PseudoCodeToPythonVisitor(PseudoCodeVisitor):
         self.indent_level = 0
         self.declared_variables = {}
 
+    def indent(self):
+
+        return "    " * self.indent_level
+
+    def visitProgram(self, ctx):
+
+        result = []
+
+        for statement in ctx.statement_list().statement():
+            result.append(self.visit(statement))
+
+        return "\n".join(result)
+
+    def visitDeclaration(self, ctx):
+
+        var_name = ctx.IDENTIFIER().getText()
+        data_type = ctx.data_type().getText()
+
+        if not self.declared_variables.get(var_name):
+            self.declared_variables[var_name] = data_type
+
+            if data_type in ["INTEGER", "REAL", "STRING", "BOOLEAN"]:
+                return ""
+            if data_type.startswith("ARRAY"):
+
+                return self.handleArrayDeclaration(var_name, data_type)
+
+        else:
+
+            return ""
+
+    def handleArrayDeclaration(self, var_name, data_type):
+
+        array_type_parts = data_type.split("OF", 1)
+        elements_number_part = array_type_parts[0]
+        array_type = array_type_parts[-1].strip()
+
+        elements_number = (
+            elements_number_part.replace("ARRAY[", "").replace("]", "").strip()
+        )
+
+        if array_type.startswith("ARRAY"):
+
+            inner_array_declaration = self.handleArrayDeclaration(
+                var_name, array_type
+            ).replace(f"{var_name} = ", "")
+
+            return f"{var_name} = [{inner_array_declaration} for _ in range({elements_number})]"
+
+        match array_type:
+            case "INTEGER":
+                x = 0
+            case "REAL":
+                x = 0.0
+            case "BOOLEAN":
+                x = False
+            case _:
+                x = 0
+
+        return f"{var_name} = [{x} for _ in range({elements_number})]"
+
 
 def translate_pseudocode_to_python(input_file, output_file):
 
