b71408d840ca05760de4cc1e2780e0e898f88543
dmarek03 <domarek@student.agh.edu.pl>
2025-01-16 13:23:29 +0100

    add output_6 file

---
+++ PseudocodeToPythonTranspiler/output_files/output_6.py
@@ -0,0 +1,6 @@
+# Handling error in for loop
+NumRows = input('Enter data: ')
+# Error in visitFor_loop: 'NoneType' object has no attribute 'accept' 
+for j in range(0, i):
+    print(f"i:{i}")
+print(f"{'\n'}")
